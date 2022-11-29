adb2b10c629c1503853eb0bf340547bf3239d973
Riezzo, Marco (contracted) <marco.riezzo@contracted.pmi.com>
2022-11-29 10:27:19 +0100

    moved from sqlite to posgres

--- telegram_handler/telegram_handler.py
+++ telegram_handler/telegram_handler.py
@@ -1,5 +1,5 @@
-import sqlite3
-
+import psycopg2
+import os
 from telegram import ParseMode
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
@@ -8,23 +8,28 @@ from price_tracker.shop_factory import ShopFactory
 
 class TelegramHandler(object):
     def __init__(self):
-        self.db_name = "watch.db"
+        self.db_conn = os.getenv("DB_CONN")
         pass
 
+    def get_connection(self):
+        conn = psycopg2.connect(self.db_conn)
+        return conn
+
     def init_schema(self):
-        con = sqlite3.connect(self.db_name, check_same_thread=False)
-        con.execute("""
+        con = self.get_connection()
+        cur = con.cursor()
+        cur.execute("""
             CREATE TABLE IF NOT EXISTS "user" (
                 user_id INTEGER,
                 name TEXT,
                 CONSTRAINT user_PK PRIMARY KEY (user_id)
             );
         """)
-        con.execute("""
+        cur.execute("""
             CREATE TABLE IF NOT EXISTS watch (
                 user_id INTEGER,
                 url TEXT,
-                desired_price NUMERIC,
+                desired_price real,
                 CONSTRAINT watch_PK PRIMARY KEY (user_id,url),
                 CONSTRAINT watch_FK FOREIGN KEY (user_id) REFERENCES "user"(user_id)
             );
@@ -33,17 +38,22 @@ class TelegramHandler(object):
         con.close()
 
     def add_user(self, user_id, user_name):
-        con = sqlite3.connect(self.db_name, check_same_thread=False)
-        if len(con.execute(f"SELECT user_id FROM user WHERE user_id = {user_id}").fetchall()) == 0:
-            con.execute(f"INSERT INTO user (user_id, name) VALUES ({user_id}, '{user_name}')")
+        con = self.get_connection()
+        cur = con.cursor()
+        cur.execute(f"SELECT user_id FROM public.user WHERE user_id = {user_id}")
+        if len(cur.fetchall()) == 0:
+            cur.execute(f"INSERT INTO public.user (user_id, name) VALUES ({user_id}, '{user_name}')")
             con.commit()
+            con.close()
 
     def add_watch_for_user(self, user_id, url, desired_price):
-        con = sqlite3.connect(self.db_name, check_same_thread=False)
-        if len(con.execute(
-                f"SELECT user_id, url FROM watch WHERE user_id = {user_id} and url = '{url}'").fetchall()) == 0:
-            con.execute(f"INSERT INTO watch (user_id, url, desired_price) VALUES ({user_id}, '{url}', {desired_price})")
+        con = self.get_connection()
+        cur = con.cursor()
+        cur.execute(f"SELECT user_id, url FROM public.watch WHERE user_id = {user_id} and url = '{url}'")
+        if len(cur.fetchall()) == 0:
+            cur.execute(f"INSERT INTO watch (user_id, url, desired_price) VALUES ({user_id}, '{url}', {desired_price})")
             con.commit()
+            con.close()
 
     @staticmethod
     def list_products(context, watched_urls_dict: dict, user_id, only_below_desired_price=False, intro=""):
@@ -68,15 +78,29 @@ class TelegramHandler(object):
                 ]
                 reply_markup = InlineKeyboardMarkup(keyboard)
                 context.bot.send_message(chat_id=user_id, text=message, parse_mode=ParseMode.HTML, reply_markup=reply_markup)
+        failed_urls = sf.get_failed_urls()
+        unhandled_urls = sf.get_unhandled_urls()
+        if failed_urls:
+            print("FAILED URLS:")
+            print(failed_urls)
+        if unhandled_urls:
+            print("UNHANDLED URLS:")
+            print(unhandled_urls)
 
     def get_all_watched_urls_by_user(self, user_id):
-        con = sqlite3.connect(self.db_name, check_same_thread=False)
-        watched_urls = con.execute(f"SELECT url, desired_price FROM watch WHERE user_id = {user_id}").fetchall()
+        con = self.get_connection()
+        cur = con.cursor()
+        cur.execute(f"SELECT url, desired_price FROM public.watch WHERE user_id = {user_id}")
+        watched_urls = cur.fetchall()
+        con.close()
         return watched_urls
 
     def get_all_users(self):
-        con = sqlite3.connect(self.db_name, check_same_thread=False)
-        users = con.execute("SELECT DISTINCT user_id FROM watch").fetchall()
+        con = self.get_connection()
+        cur = con.cursor()
+        cur.execute("SELECT DISTINCT user_id FROM public.watch")
+        users = cur.fetchall()
+        con.close()
         users = [user[0] for user in users]
         return users
 
